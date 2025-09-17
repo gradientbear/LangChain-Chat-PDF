aeab9b01062bad21eb7206e65562a998d084ac1a
gradientbear <jack.h.tamm@gmail.com>
2025-09-17 01:12:05 -0400

    upload pdf

---
+++ app.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+
+def main():
+  load_dotenv()
+  st.set_page_config(page_title="Ask your PDF")
+  st.header("Ask your PDF!!!")
+
+  pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+  if pdf is not None:
+    pdf_reader = PdfReader(pdf)
+
+if __name__ == '__main__':
+  main()
